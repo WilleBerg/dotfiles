46b9952487cc82d14ab6afb09239e443a58db3bc
William Berg <william.berg2@gmail.com>
2025-02-11 08:50:50 +0100

    Fixed bugs

--- projects/lockfree.sh
+++ projects/lockfree.sh
@@ -6,15 +6,13 @@ if tmux has-session -t lockfree 2>/dev/null; then
         tmux attach -t lockfree
     fi
 else
-    cd $HOME/lockfree-benchmark
-    tmux new-session -d -s lockfree
-    tmux split-window -h
-    tmux resize-pane -R 50
+    cd $HOME/lockfree-benchmark && tmux new-session -d -s lockfree
+    tmux split-window -t lockfree -h
+    tmux resize-pane -t lockfree -R 50
     tmux select-pane -t 1
     tmux send-keys -t lockfree:1.1 'nvim src/lib.rs' C-m
     tmux new-window -t lockfree:2
     tmux send-keys -t lockfree:2 'nvim Cargo.toml' C-m
-    tmux select-window -t 1
     if [ -n "$TMUX" ]; then
         tmux switch-client -t lockfree
     else
